a4502db89122028b1356beb243f9248c0e9453aa
Ikenna Nwagbara <ikenna.nwagbara@gmail.com>
2025-01-12 23:21:08 -0500

    changed exchange to name

--- migrations/2024-12-27-005559_create_exchanges/up.sql
+++ migrations/2024-12-27-005559_create_exchanges/up.sql
@@ -2,5 +2,5 @@
 CREATE TABLE IF NOT EXISTS exchanges (
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     exchange_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    exchange VARCHAR(8) UNIQUE NOT NULL
+    name VARCHAR(8) UNIQUE NOT NULL
 );
