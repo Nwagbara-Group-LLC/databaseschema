fe9ecbcd1a6d23f1f98c0d230239eac7adad7ca5
Ikenna Nwagbara <ikenna.nwagbara@gmail.com>
2025-01-13 09:27:02 -0500

    Removed unique constraint on exchanges

--- migrations/2024-12-27-005607_create_order_books/up.sql
+++ migrations/2024-12-27-005607_create_order_books/up.sql
@@ -2,10 +2,10 @@
 CREATE TABLE IF NOT EXISTS order_books (
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at TIMESTAMPTZ DEFAULT now(),
-    symbol VARCHAR(7) NOT NULL REFERENCES securities (symbol),
+    symbol VARCHAR(7) UNIQUE NOT NULL REFERENCES securities (symbol),
     exchange VARCHAR(8) NOT NULL REFERENCES exchanges (exchange),
     security_id UUID UNIQUE NOT NULL REFERENCES securities (security_id),
-    exchange_id UUID UNIQUE NOT NULL REFERENCES exchanges (exchange_id),
+    exchange_id UUID NOT NULL REFERENCES exchanges (exchange_id),
     order_book_id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
     buy_order_book_id UUID UNIQUE NOT NULL DEFAULT gen_random_uuid(),
     sell_order_book_id UUID UNIQUE NOT NULL DEFAULT gen_random_uuid(),
