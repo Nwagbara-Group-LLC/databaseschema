6791726c3a7509e194cff3382adf1ada7334e65a
Ikenna Nwagbara <ikenna.nwagbara@gmail.com>
2025-01-13 01:36:13 -0500

    removed unique contraint on exchange

--- migrations/2024-12-27-005607_create_order_books/up.sql
+++ migrations/2024-12-27-005607_create_order_books/up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS order_books (
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at TIMESTAMPTZ DEFAULT now(),
     symbol VARCHAR(7) UNIQUE NOT NULL REFERENCES securities (symbol),
-    exchange VARCHAR(8) UNIQUE NOT NULL REFERENCES exchanges (exchange),
+    exchange VARCHAR(8) NOT NULL REFERENCES exchanges (exchange),
     security_id UUID UNIQUE NOT NULL REFERENCES securities (security_id),
     exchange_id UUID UNIQUE NOT NULL REFERENCES exchanges (exchange_id),
     order_book_id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
